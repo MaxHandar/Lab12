e32ed8b7b05b7c9c05fb0cb3c72710b83d4b2e1b
handa <handa@LAPTOP-PMA64J4J>
2024-06-23 09:55:59 +0500

    13 лаба все тесты

---
+++ UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a5d6e04b-a10b-449f-83bb-280dff3bbfae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Lab12_4;
+using ClassLibraryLab10;
+using System.Collections.ObjectModel;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class MyObservableCollectionTests
+    {
+        private MyObservableCollection<Vehicle> collection;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Инициализация коллекции перед каждым тестом
+            collection = new MyObservableCollection<Vehicle>();
+        }
+
+        [TestMethod]
+        public void Add_Item_Should_Trigger_CollectionCountChanged()
+        {
+            // Arrange
+            var vehicle = new Vehicle();
+            bool eventFired = false;
+            collection.CollectionCountChanged += (sender, args) => eventFired = true;
+
+            // Act
+            collection.Add(vehicle);
+
+            // Assert
+            Assert.IsTrue(eventFired);
+        }
+
+        [TestMethod]
+        public void Remove_Item_Should_Trigger_CollectionCountChanged()
+        {
+            // Arrange
+            var vehicle = new Vehicle();
+            collection.Add(vehicle);
+            bool eventFired = false;
+            collection.CollectionCountChanged += (sender, args) => eventFired = true;
+
+            // Act
+            collection.Remove(vehicle);
+
+            // Assert
+            Assert.IsTrue(eventFired);
+        }
+
+        [TestMethod]
+        public void Set_Item_Should_Trigger_CollectionReferenceChanged()
+        {
+            // Arrange
+            var vehicle = new Vehicle();
+            collection.Add(vehicle);
+            bool eventFired = false;
+            collection.CollectionReferenceChanged += (sender, args) => eventFired = true;
+
+            // Act
+            collection[0] = new Vehicle();
+
+            // Assert
+            Assert.IsTrue(eventFired);
+        }
+
+        [TestMethod]
+        public void Collection_Should_Be_Empty_After_Clear()
+        {
+            // Arrange
+            collection.Add(new Vehicle());
+            collection.Add(new Vehicle());
+
+            // Act
+            collection.Clear();
+
+            // Assert
+            Assert.AreEqual(0, collection.Length);
+        }
+
+    }
+}
